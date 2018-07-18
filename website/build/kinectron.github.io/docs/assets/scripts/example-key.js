38708ac0a70335dbf0d673821aff6bfb24f1b907
lisajamhoury <lisajamhoury@gmail.com>
2018-07-17 22:33:46 -0400

    Joint example working natively

---
+++ website/build/kinectron.github.io/docs/assets/scripts/example-key.js
@@ -0,0 +1,99 @@
+// Run with simplehttpserver for image to load properly. http://www.andyjamesdavies.com/blog/javascript/simple-http-server-on-mac-os-x-in-seconds
+
+// Set to true if using live kinectron data
+let liveData = false;
+
+// declare IP address, FILL HERE INFO FROM CLIENT
+let kinectronIpAddress = "10.0.1.12";
+
+// Declare kinectron
+let kinectron = null;
+
+// hold beach image
+let beach;
+
+// recorded data variables
+let sentTime = Date.now();
+let currentFrame = 0;
+
+// an array to hold recorded Kinectron images
+let recordedImgs = [];
+
+
+function preload() {
+  beach = loadImage("./assets/images/beach.png");
+}
+
+
+function setup() {
+  let canvas = createCanvas(640, 426);
+  canvas.parent('p5-sketch'); 
+
+  background(255);
+
+  if (!liveData) {
+    textSize(18);
+    text(`
+    Loading... give it a good 10 seconds... 
+    (but this may not work if you have adblock installed)`, 50, 50);
+
+    // turn recorded kinectron video into images so they are "served" like kinectron feed
+    videoToImages('./assets/images/vid_black.webm')
+      .then(function(returnedImgs) {
+        recordedImgs = returnedImgs;
+      });
+  }
+
+  if (liveData) initKinectron();
+
+}
+
+
+function draw() {
+  
+  // if we're running from recorded video and the recorded images have loaded, loop the recorded images
+  if (!liveData && typeof recordedImgs !== 'undefined' && recordedImgs.length > 0) {
+    loopRecordedData();
+  }
+}
+
+
+function goToBeach(img) {
+  // once a kinectron image is loaded
+  loadImage(img.src, function(loadedImage) {
+
+    //draw the beach image, then the kinectron key image
+    image(beach, 0, 0);
+    image(loadedImage, 0, 0);
+  });
+}
+
+
+function initKinectron() {
+  // Define and create an instance of kinectron
+  kinectron = new Kinectron(kinectronIpAddress);
+
+  // Connect with application over peer
+  kinectron.makeConnection();
+
+  // Start the greenscreen camera
+  kinectron.startKey(goToBeach);
+
+}
+
+function loopRecordedData() {
+
+  // send data every 20 ms
+  if (Date.now() > sentTime + 40) {
+    goToBeach(recordedImgs[currentFrame])
+    sentTime = Date.now();
+
+    if (currentFrame < recordedImgs.length-1) {
+      currentFrame++;
+    } else {
+      currentFrame = 0;
+    }
+
+  }
+
+}
---
+++ website/build/kinectron.github.io/docs/assets/scripts/example-simple-skeleton.js
@@ -0,0 +1,207 @@
+// set to true if using live kinectron data
+let liveData = false;
+
+// fill in kinectron ip address here ie. "127.16.231.33"
+let kinectronIpAddress = ""; 
+
+// declare kinectron 
+let kinectron = null;
+
+// drawHand variables
+let start = 30;
+let target = 100;
+let diameter = start;
+let light = 255;
+let dark = 100;
+let hueValue = light;
+let lerpAmt = 0.3;
+let state = 'ascending';
+
+// recorded data variables
+let sentTime = Date.now();
+let currentFrame = 0;
+let recorded_skeleton;
+let recorded_data_file = "./assets/data/recorded_skeleton.json";
+
+
+function preload() {
+  console.log('p');
+  
+  if (!liveData) {
+    recorded_skeleton = loadJSON(recorded_data_file, "json");
+  }
+
+}
+
+function setup() {
+  let canvas = createCanvas(500, 500);
+  canvas.parent('p5-sketch');
+  background(0);
+  noStroke();
+
+  if (liveData) initKinectron();
+}
+
+
+function draw() {
+
+  if (!liveData) loopRecordedData();
+}
+
+
+function loopRecordedData() {
+  
+  // send data every 20 seconds 
+  if (Date.now() > sentTime + 20) {
+    bodyTracked(recorded_skeleton[currentFrame])
+    sentTime = Date.now();
+
+    if (currentFrame < Object.keys(recorded_skeleton).length-1) {
+      currentFrame++;
+    } else {
+      currentFrame = 0;
+    }
+  }
+
+}
+
+function initKinectron() {
+  // define and create an instance of kinectron
+  kinectron = new Kinectron(kinectronIpAddress);
+
+  // connect with application over peer
+  kinectron.makeConnection();
+
+  // request all tracked bodies and pass data to your callback
+  kinectron.startTrackedBodies(bodyTracked);
+
+}
+
+
+function bodyTracked(body) {
+  background(0, 20);
+
+  let hands = [];
+
+  // get all the joints off the tracked body and do something with them
+  for(let jointType in body.joints) {
+    joint = body.joints[jointType];
+
+    drawJoint(joint);
+
+    // get the hands off the tracked body and do somethign with them
+    
+    // find right hand 
+    if (jointType == 11) {
+      hands.rightHand = joint;
+      hands.rightHandState = translateHandState(body.rightHandState);
+    }
+
+    // find left hand
+    if (jointType == 7) {
+      hands.leftHand = joint;
+      hands.leftHandState = translateHandState(body.leftHandState);
+    }
+
+  }
+
+  drawHands(hands);
+
+}
+
+// draw skeleton
+function drawJoint(joint) {
+  fill(100);
+
+  // kinect location data needs to be normalized to canvas size
+  ellipse(joint.depthX * width, joint.depthY * height, 15, 15);
+
+  fill(200);
+
+  // kinect location data needs to be normalized to canvas size
+  ellipse(joint.depthX * width, joint.depthY * height, 3, 3);
+}
+
+// make handstate more readable
+function translateHandState(handState) {
+  switch (handState) {
+    case 0:
+      return 'unknown';
+
+    case 1:
+      return 'notTracked';
+
+    case 2:
+      return 'open';
+
+    case 3:
+      return 'closed';
+
+    case 4:
+      return 'lasso';
+  }
+}
+
+
+// draw hands
+function drawHands(hands) {
+
+  // check if hands are touching 
+  if ((Math.abs(hands.leftHand.depthX - hands.rightHand.depthX) < 0.01) && (Math.abs(hands.leftHand.depthY - hands.rightHand.depthY) < 0.01)) {
+    hands.leftHandState = 'clapping';
+    hands.rightHandState = 'clapping';
+  }
+
+  // draw hand states
+  updateHandState(hands.leftHandState, hands.leftHand);
+  updateHandState(hands.rightHandState, hands.rightHand);
+}
+
+// find out state of hands
+function updateHandState(handState, hand) {
+
+  switch (handState) {
+    case 'closed':
+      drawHand(hand, 1, 255);
+      break;
+
+    case 'open':
+      drawHand(hand, 0, 255);
+      break;
+
+    case 'lasso':
+      drawHand(hand, 0, 255);
+      break;
+
+      // create new state for clapping
+    case 'clapping':
+      drawHand(hand, 1, 'red');
+  }
+}
+
+// draw the hands based on their state
+function drawHand(hand, handState, color) {
+
+  if (handState === 1) {
+    state = 'ascending';
+  }
+
+  if (handState === 0) {
+    state = 'descending';
+  }
+
+  if (state == 'ascending') {
+    diameter = lerp(diameter, target, lerpAmt);
+    hueValue = lerp(hueValue, dark, lerpAmt);
+  }
+
+  if (state == 'descending') {
+    diameter = lerp(diameter, start, lerpAmt);
+    hueValue = lerp(hueValue, light, lerpAmt);
+  }
+
+  fill(color);
+
+  // Kinect location needs to be normalized to canvas size
+  ellipse(hand.depthX * width, hand.depthY * height, diameter, diameter);
+}
